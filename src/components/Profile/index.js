e38dfad6a4461c880310afb60026c0cd05e94b36
Mateus <mateus4k@protonmail.ch>
2019-12-12 21:55:25 -0300

    Create about page

---
+++ src/components/Profile/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="container">
+    <h1>Mateus</h1>
+    <h2>Developer</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+      commodi vero esse alias hic optio, asperiores error voluptates nesciunt
+      laboriosam aspernatur incidunt natus rerum ad sed voluptas nobis eveniet
+      culpa!
+    </p>
+  </div>
+)
+
+export default Profile
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <main>{children}</main>
-    </>
-  )
-}
+import Profile from "./Profile"
+
+const Layout = ({ children }) => (
+  <>
+    <aside>
+      <Profile />
+    </aside>
+    <main>{children}</main>
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
