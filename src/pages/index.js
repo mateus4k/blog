8016442fcfe3626dbdeb1651e33a4d5a2f327137
Mateus <mateus4k@protonmail.ch>
2019-12-13 18:53:48 -0300

    Refactor post item mockup content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import PostItem from "../components/PostItem"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
-    <li>
-      <Link to="/" activeStyle={{ color: "red" }}>
-        Home
-      </Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
   </Layout>
 )
 
